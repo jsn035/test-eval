57d51f01cc1ee509ce1ec5721778d2f3cc143577
jsn035 <jeremy.salmon@gmx.fr>
2024-10-17 21:42:28 +0200

    layout

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="my-3">
+	<div class="container my-3">
+		<div class="text-center bg-primary-subtle p-3 rounded">Footer</div>
+	</div>
+</footer>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header class="my-3">
+	<div class="container my-3">
+		<div class="bg-primary-subtle text-center p-3 rounded">{{ env('APP_TITLE') }}</div>
+	</div>
+</header>
