737a4ae30bc21a1318e04e0bcd2fd1b3253478b0
jsn035 <jeremy.salmon@gmx.fr>
2024-10-17 21:44:32 +0200

    vues trendings

---
+++ resources/views/trendings/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts/app')
+
+@section('content')
+<section id="trendings" class="my-3">
+	<div class="container my-3">
+		<div class="row row-cols-3 g-3">
+			@foreach($trendings->items() as $item)
+			<div class="col">
+				<div class="px-3 bg-body text-center border rounded">
+					<!-- title -->
+					<h1 class="fs-3 my-3 text-nowrap"><a href="{{ url("/trendings/{$item->id}") }}">{{ $item->title }}</a></h1>
+
+					<!-- poster -->
+					<div class="my-3">
+						<a href="#"><img src="https://image.tmdb.org/t/p/w500/{{ $item->poster_path }}" alt="{{ $item->title }}" class="w-100 border"></a>
+					</div>
+
+					<!-- vote -->
+					<div class="my-3 text-nowrap">Note : {{ $item->vote_average }}</div>
+				</div>
+			</div>
+			@endforeach
+		</div>
+	</div>
+
+	<div class="container my-3">
+		{{ $trendings->links('layouts/partials/paginator') }}
+	</div>
+</section>
+@endsection
---
+++ resources/views/trendings/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts/app')
+
+@section('content')
+<section id="trending" class="my-3">
+	<div class="container">
+		<div class="container bg-body border rounded">
+			<div class="row g-3">
+				<!-- poster -->
+				<div class="col-3">
+					<div class="text-center px-3">
+						<div class="my-3">
+							<a href="https://image.tmdb.org/t/p/original/{{ $trending->poster_path }}"><img src="https://image.tmdb.org/t/p/w500/{{ $trending->poster_path }}" alt="{{ $trending->title }}" class="w-100 border"></a>
+						</div>
+
+						<div class="my-3"><a href="https://www.themoviedb.org/movie/{{ $trending->id }}">Voir sur TMDB</a></div>
+					</div>
+				</div>
+
+				<!-- détails -->
+				<div class="col-9">
+					<div class="px-3">
+						<!-- tous -->
+						<div class="my-3">
+							<a href="{{ url("/trendings") }}">&larr; Tous</a>
+						</div>
+
+						<!-- title -->
+						<div class="my-3">
+							<h1 class="my-0">{{ $trending->title }}</h1>
+							<em>{{ $trending->original_title }} ({{ $trending->original_language }})</em>
+						</div>
+
+						<hr>
+
+						<!-- overview -->
+						<div class="my-3">
+							<div class="my-3">
+								<em><strong>Type : </strong>{{ $trending->media_type }}</em><br>
+								<em><strong>Note :</strong> {{ $trending->vote_average }} / 10 ({{ $trending->vote_count }} votes)</em>
+							</div>
+
+							<p><strong></strong>{{ $trending->overview }}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+</section>
+@endsection
