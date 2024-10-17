e0df76c906e95967cd7e7b6ef56eb4ef81068a72
jsn035 <jeremy.salmon@gmx.fr>
2024-10-17 21:43:35 +0200

    routes trendings

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TrendingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+Route::resource('trendings', TrendingsController::class);
