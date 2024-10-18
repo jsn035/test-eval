cd045eb5cf9767e29d0f2c200421ebb49376b590
jsn035 <jeremy.salmon@gmx.fr>
2024-10-18 12:56:54 +0200

    vue dashboard trendings index

---
+++ resources/views/dashboard/trendings/index.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+			{{ __('Trendings') }}
+		</h2>
+	</x-slot>
+
+	<div class="py-12">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+				<table class="w-full text-sm text-left rtl:text-right text-gray-500">
+					<thead class="text-xs text-gray-700 uppercase bg-gray-50">
+						<tr>
+							<th scope="col" class="px-6 py-3">&nbsp;</th>
+							<th scope="col" class="px-6 py-3">Titre</th>
+							<th scope="col" class="px-6 py-3">Type</th>
+							<th scope="col" class="px-6 py-3">Langue originale</th>
+							<th scope="col" class="px-6 py-3">Popularité</th>
+							<th scope="col" class="px-6 py-3">Note moyenne</th>
+							<th scope="col" class="px-6 py-3">Nb. votes</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						@foreach($trendings as $trending)
+						<tr class="bg-white border-b">
+							<td class="px-6 py-4"><a href="{{ url("/dashboard/trendings/{$trending->id}") }}" class="text-center text-white cursor-pointer bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Éditer</a></td>
+							<td class="px-6 py-4">{{ $trending->title }}<br><em>{{ $trending->original_title }}</em></td>
+							<td class="px-6 py-4">{{ $trending->media_type }}</td>
+							<td class="px-6 py-4">{{ $trending->original_language }}</td>
+							<td class="px-6 py-4">{{ $trending->popularity }}</td>
+							<td class="px-6 py-4">{{ $trending->vote_average }}</td>
+							<td class="px-6 py-4">{{ $trending->vote_count }}</td>
+						</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</x-app-layout>
