c188efe44b1136ee1fc2f5678b3fd4f7e163f781
jsn035 <jeremy.salmon@gmx.fr>
2024-10-18 11:26:50 +0200

    liens dashboard

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,5 +1,26 @@
 <header class="my-3">
 	<div class="container my-3">
-		<div class="bg-primary-subtle text-center p-3 rounded">{{ env('APP_TITLE') }}</div>
+		<div class="d-flex gap-3 bg-primary-subtle p-3 rounded">
+			<div class="flex-grow-1">{{ env('APP_TITLE') }}</div>
+
+			@if (Route::has('login'))
+			@auth
+			<div>
+				<a href="{{ url('/dashboard') }}">Dashboard</a>
+			</div>
+
+			@else
+			<div>
+				<a href="{{ route('login') }}">Log in</a>
+			</div>
+		
+			@if (Route::has('register'))
+			<div>
+				<a href="{{ route('register') }}">Register</a>
+			</div>
+			@endif
+			@endauth
+			@endif
+		</div>
 	</div>
 </header>
