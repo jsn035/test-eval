b4e74361a127be3e25c0d141b117d48686d26999
jsn035 <jeremy.salmon@gmx.fr>
2024-10-18 12:54:51 +0200

    routes dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,36 @@
 <?php
 
-use App\Http\Controllers\TrendingsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [
+	App\Http\Controllers\TrendingsController::class,
+	'index'
+]);
 
-Route::get('/', [TrendingsController::class, 'index']);
-
-Route::resource('trendings', TrendingsController::class);
+Route::resource(
+	'trendings',
+	App\Http\Controllers\TrendingsController::class
+);
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+	'auth:sanctum',
+	config('jetstream.auth_session'),
+	'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+	Route::prefix('dashboard')
+		->group(function () {
+			Route::get('/', function () {
+				return view('dashboard');
+			})->name('dashboard');
+
+			Route::get('/trendings', [
+				App\Http\Controllers\Dashboard\TrendingsController::class,
+				'index'
+			])->name('dashboard-trendings');
+
+			Route::resource(
+				'trendings',
+				App\Http\Controllers\Dashboard\TrendingsController::class
+			);
+		});
 });
